cbe668e5de87ae78fc5a98a12651361c3b5fa01e
SujalParyani404 <104881048+sujalparyani@users.noreply.github.com>
2023-03-03 20:50:44 +0530

    Assignment of 20/03/2023 is complete

---
+++ 02/03/2023.py
@@ -0,0 +1,5 @@
+principle= int(input("Enter principle amount : "))
+time = int(input("Enter time : "))
+rate = int(input("Enter rate : "))
+simple = (principle*time*rate)/100
+print("Your answer is :",simple)
